6a349215ea68c57be7de9da6bed11de57153ae16
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-03-28 11:19:55 -0600

    setting up project

---
+++ Microservice-A/src/test/java/UnderstandingMicroservices/UnderstandingMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package UnderstandingMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
