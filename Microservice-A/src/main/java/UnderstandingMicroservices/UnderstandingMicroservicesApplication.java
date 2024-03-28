c30eb02f1ba849c7f93794694d77f6fdc1bd76e0
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-03-28 12:34:06 -0600

    setting up app

---
+++ Microservice-A/src/main/java/UnderstandingMicroservices/UnderstandingMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package UnderstandingMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderstandingMicroservicesApplication.class, args);
+	}
+
+}
